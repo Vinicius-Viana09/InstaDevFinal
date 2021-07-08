4a47a75f7af990c52006a76828f774d6611adae3
maatheuss0 <matheusaraujoms.machado@gmail.com>
2021-07-07 22:08:13 -0300

    odirlei salva nois

--- Controllers/FeedController.cs
+++ Controllers/FeedController.cs
@@ -8,11 +8,27 @@ namespace InstaDevFinal.Controllers
         Post Postmodel = new Post();
 
         Usuario Usuariomodel = new Usuario();
-        public IActionResult index(){
+        public IActionResult index()
+        {
             ViewBag.Posts = Postmodel.LerTodosPost();
             ViewBag.Usuarios = Usuariomodel.LerTodosUsuarios();
             return View();
         }
+
+        // public IActionResult Postar()
+        // {
+        //     Jogador novoJogador = new Jogador();
+        //     novoJogador.Id_Jogador = Int32.Parse(form["IdJogador"]);
+        //     novoJogador.Id_Equipe = Int32.Parse(form["IdEquipe"]);
+        //     novoJogador.Nome = form["Nome"];
+        //     novoJogador.Email = form["Email"];
+        //     novoJogador.Senha = form["Senha"];
+
+        //     JogadorModel.Criar(novoJogador);
+        //     ViewBag.Jogadores = JogadorModel.LerTodos();
+
+        //     return LocalRedirect("~/Jogador/Index");
+        // }
 
     }
 }
